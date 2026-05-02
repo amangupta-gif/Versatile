954282cf9f1ba8a91a3d4e4ff174ac489394fbb0
conqueror biker <shivay.lfc@gmail.com>
2026-05-02 12:44:39 +0530

    temp

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -82,7 +82,7 @@ export default function HomePage({ setPage }) {
               color: "var(--text-primary)",
             }}
           >
-            Empowering India's Future Through{" "}
+            Declining India's Future Through{" "}
             <span style={{ color: "var(--accent)" }}>Skills & Innovation</span>
           </h1>
 
